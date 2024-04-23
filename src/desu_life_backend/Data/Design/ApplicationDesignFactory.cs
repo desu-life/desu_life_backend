3fb3c04d5826ac76948ab71e84a32742c5741de4
Milkitic <Milkitic@126.com>
2024-04-23 17:08:38 +0800

    Add migration

---
+++ src/desu_life_backend/Data/Design/ApplicationDesignFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace desu.life.Data.Design;
+
+public class ApplicationDesignFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseMySql(ServerVersion.AutoDetect(""));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
