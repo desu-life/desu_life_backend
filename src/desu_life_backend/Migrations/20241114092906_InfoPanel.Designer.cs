c15c5443fdc747643e797abcf33c1b75df2b1eba
quhansheng <quhansheng@gmail.com>
2024-11-14 17:32:29 +0800

    生成数据库Migration

--- src/desu_life_backend/Migrations/20241114092906_InfoPanel.Designer.cs
+++ src/desu_life_backend/Migrations/20241114092906_InfoPanel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using desu.life.Data;
 
@@ -11,9 +12,11 @@ using desu.life.Data;
 namespace desu.life.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241114092906_InfoPanel")]
+    partial class InfoPanel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/desu_life_backend/Migrations/20241114092906_InfoPanel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace desu.life.Migrations
+{
+    /// <inheritdoc />
+    public partial class InfoPanel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InfoPanel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "主键")
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false, comment: "Panel标题")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Content = table.Column<string>(type: "longtext", nullable: false, comment: "Panel HTML内容")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Audited = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: "是否审核通过"),
+                    Applied = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: "是否已应用"),
+                    CreationTime = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "创建时间"),
+                    LastModifiedTime = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "修改时间"),
+                    AuthorId = table.Column<int>(type: "int", nullable: false, comment: "创建者ID")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoPanel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfoPanel_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoPanel_AuthorId",
+                table: "InfoPanel",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfoPanel");
+        }
+    }
+}
